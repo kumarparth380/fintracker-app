bfff76487e9a0be2ff89eff5b59e9910fcbb295b
kumarparth380 <kumarparth380@gmail.com>
2025-02-28 14:00:18 +0530

    feat: add a common layout

---
+++ src/Layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from '@/components/Header';
+import { Sidebar } from '@/components/Sidebar';
+
+const Layout = ({ children }: { children: React.ReactElement }) => (
+  <div className="flex min-h-screen">
+    <Sidebar />
+    <div className="flex-1 lg:pl-[280px]">
+      <Header />
+      {children}
+    </div>
+  </div>
+);
+
+export default Layout;
