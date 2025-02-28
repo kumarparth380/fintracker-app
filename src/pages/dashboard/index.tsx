e9194fffb571d7a79c62d8f81c96d1be3766037b
kumarparth380 <kumarparth380@gmail.com>
2025-02-28 14:54:00 +0530

    feat: dashboard page initials

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { CardsList } from '@/components/dashboard/CardsList';
 
 export default function DashboardPage() {
   return (
-    <div>
-      <p>Dashboard Page</p>
+    <div className="min-h-screen bg-[#F8F9FB]">
+      <div className="p-4 pt-[40px] lg:p-8 lg:pt-[40px]">
+        <div className="grid grid-cols-1 items-stretch gap-6 md:gap-8 lg:grid-cols-12">
+          <div className="flex flex-col lg:col-span-8">
+            <CardsList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/types/card.ts
@@ -0,0 +1,7 @@
+export interface Card {
+  balance: number;
+  name: string;
+  validity: string;
+  number: string;
+  type: string;
+}
